1e04d3910f9675300e337b98259fe10cda1f2512
Aleksandra Gencheva <a.gencheva@student.fontys.nl>
2021-07-15 14:15:33 +0200

    implemented account activation

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -27,7 +27,7 @@ module SessionsHelper
         elsif (user_id = cookies.encrypted[:user_id])
             user = User.find_by(id: user_id)
 
-            if user&.authenticated?(cookies[:remember_token])
+            if user&.authenticated?(:remember, cookies[:remember_token])
                 login user
                 @current_user = user
             end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'aleksandra.gencheva@yahoo.com'
   layout 'mailer'
 end
